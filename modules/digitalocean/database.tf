6a42bae285bb0241eee17fea813597852fd5e1b8
vuvannghiawork <vuvannghia.work@gmail.com>
2026-05-03 20:31:12 +0700

    version database postgres vbpl (#31)

--- modules/digitalocean/database.tf
+++ modules/digitalocean/database.tf
@@ -2,7 +2,7 @@
 resource "digitalocean_database_cluster" "postgres_db" {
   name       = "prod-postgres-cluster"
   engine     = "pg"
-  version    = "15"
+  version    = "17" # Nâng lên 17 để đồng bộ với Neon
   size       = "db-s-1vcpu-1gb" # Node cơ bản nhất ($15/tháng)
   region     = var.region
   node_count = 1
