80201575d12b9d21827a7720d2815055ca854cc0
vuvannghiawork <vuvannghia.work@gmail.com>
2026-05-07 21:05:43 +0700

    1

--- modules/neon/variables.tf
+++ modules/neon/variables.tf
@@ -10,16 +10,19 @@ variable "NEON_ORGANIZATION_ID" {
   default     = "org-still-feather-82034197"
 }
 
+variable "setting_service_projects" {
+  type        = list(string)
+  description = "Danh sách project cho setting service"
+  default = [
+    "dev-setting-service",
+    "prod-setting-service"
+  ]
+}
+
 variable "projects" {
   type        = list(string)
   description = "Danh sách tên các project Neon cần tạo"
   default = [
-    "dev-api-gateway",
-    "prod-api-gateway",
-
-    "dev-api-gateway-http-log",
-    "prod-api-gateway-http-log",
-
     "dev-database-data-pipeline-vbpl",
     "prod-database-data-pipeline-vbpl",
 
@@ -29,62 +32,29 @@ variable "projects" {
     "dev-data-pipeline-phapdien",
     "prod-data-pipeline-phapdien",
 
-
-
-
+    "dev-api-gateway",
+    "prod-api-gateway",
+    "dev-api-gateway-http-log",
+    "prod-api-gateway-http-log",
 
     "dev-payment-service",
     "prod-payment-service",
 
-
-
-
-
     "dev-conversation-service",
     "prod-conversation-service",
 
-
-
-
-
-
     "dev-chatbot-service",
     "prod-chatbot-service",
-
     "dev-chat-history",
     "prod-chat-history",
 
-
-
-
-
     # "dev-document-service",
     # "prod-document-service",
-
-
-
-
-
     # "dev-vector-database-user-documents",
     # "prod-vector-database-user-documents",
 
-
-
-
-    
-    "dev-persona-service",
-    "prod-persona-service",
-
-
-
+    # "dev-persona-service",
+    # "prod-persona-service",
   ]
 }
 
-variable "setting_service_projects" {
-  type        = list(string)
-  description = "Danh sách các project cho setting service"
-  default = [
-    "dev-setting-service",
-    "prod-setting-service"
-  ]
-}
