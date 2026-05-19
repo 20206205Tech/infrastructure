6b9e48e665cc5aeede88f1b300b2269564a0195e
vuvannghiawork <vuvannghia.work@gmail.com>
2026-05-19 13:28:08 +0700

    NO-VERIFY - 2026-05-19 13:28:08

--- modules/neon/demo-service-variables.tf
+++ modules/neon/demo-service-variables.tf
@@ -1,9 +1,14 @@
+variable "demo_service_projects" {
+  type        = list(string)
+  description = "Danh sách project cho demo service"
+  default = [
+    "dev-demo-service",
+    "prod-demo-service"
+  ]
+}
 
-# variable "setting_service_projects" {
-#   type        = list(string)
-#   description = "Danh sách project cho setting service"
-#   default = [
-#     "dev-setting-service",
-#     "prod-setting-service"
-#   ]
-# }
+variable "supabase_jwks_url" {
+  type        = string
+  description = "JWKS URL cho Supabase Authentication Provider"
+  default     = "https://cmtvbkvxwspkdstrcrfm.supabase.co/auth/v1/.well-known/jwks.json"
+}
