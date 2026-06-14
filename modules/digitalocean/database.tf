d3a3c3d1bbe471c4a7f5087ba036943ebbc06389
vuvannghiawork <vuvannghia.work@gmail.com>
2026-06-14 17:48:51 +0700

    neon-free

--- modules/digitalocean/database.tf
+++ modules/digitalocean/database.tf
@@ -1,15 +1,23 @@
-# Tạo một cụm PostgreSQL 15 Managed Database
 resource "digitalocean_database_cluster" "postgres_db" {
   name       = "prod-postgres-cluster"
   engine     = "pg"
-  version    = "17" # Nâng lên 17 để đồng bộ với Neon
-  size       = "db-s-1vcpu-1gb" # Node cơ bản nhất ($15/tháng)
+  version    = "17" 
+  size       = "db-s-1vcpu-1gb" 
   region     = var.region
   node_count = 1
 }
 
+# Tự động tạo danh sách các cơ sở dữ liệu dựa trên biến khai báo
+resource "digitalocean_database_db" "all_databases" {
+  for_each   = toset(var.databases)
+  
+  cluster_id = digitalocean_database_cluster.postgres_db.id
+  name       = each.key
+}
+
+# Output chuỗi kết nối tổng (Tùy chọn bỏ comment nếu cần lấy thông tin)
 # output "db_connection_uri" {
 #   value       = digitalocean_database_cluster.postgres_db.uri
 #   sensitive   = true
-#   description = "Chuỗi kết nối CSDL."
+#   description = "Chuỗi kết nối CSDL tổng."
 # }
--- modules/digitalocean/variables.tf
+++ modules/digitalocean/variables.tf
@@ -12,7 +12,7 @@ variable "region" {
 
 variable "droplet_size" {
   type    = string
-  default = "s-1vcpu-1gb" # Gói thấp nhất
+  default = "s-1vcpu-1gb"
 }
 
 variable "droplet_image" {
@@ -24,4 +24,24 @@ variable "OCI_SSH_PUBLIC_KEY" {
   type        = string
   description = "Public Key để SSH vào Droplet (Dùng chung với OCI cho tiện)"
   sensitive   = true
+}
+
+# Thêm biến khai báo danh sách các database cần khởi tạo
+variable "databases" {
+  type        = list(string)
+  description = "Danh sách tên các cơ sở dữ liệu cần tạo trong cụm Postgres"
+  default     = [
+    # "database_data_pipeline_vbpl",
+    # "data_pipeline_vbplnew",
+    # "data_pipeline_phapdien",
+    # "api_gateway",
+    # "api_gateway_http_log",
+    # "chat_history",
+    "nghia",
+# "prod_code_payment_service",
+# "prod_code_conversation_service",
+# "prod_code_chatbot_service",
+# "prod_code_document_service",
+# "prod_code_persona_service",
+  ]
 }
