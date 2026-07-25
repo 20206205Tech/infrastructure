90b323092d8504ef2a383465ad9c31ce4f27be6e
vuvannghiawork <vuvannghia.work@gmail.com>
2026-07-25 22:57:08 +0700

    123 (#65)

--- modules/digitalocean/database.tf
+++ modules/digitalocean/database.tf
@@ -1,23 +1,23 @@
-resource "digitalocean_database_cluster" "postgres_db" {
-  name       = "prod-postgres-cluster"
-  engine     = "pg"
-  version    = "17" 
-  size       = "db-s-1vcpu-1gb" 
-  region     = var.region
-  node_count = 1
-}
-
-# Tự động tạo danh sách các cơ sở dữ liệu dựa trên biến khai báo
-resource "digitalocean_database_db" "all_databases" {
-  for_each   = toset(var.databases)
-  
-  cluster_id = digitalocean_database_cluster.postgres_db.id
-  name       = each.key
-}
-
-# Output chuỗi kết nối tổng (Tùy chọn bỏ comment nếu cần lấy thông tin)
-# output "db_connection_uri" {
-#   value       = digitalocean_database_cluster.postgres_db.uri
-#   sensitive   = true
-#   description = "Chuỗi kết nối CSDL tổng."
+# resource "digitalocean_database_cluster" "postgres_db" {
+#   name       = "prod-postgres-cluster"
+#   engine     = "pg"
+#   version    = "17" 
+#   size       = "db-s-1vcpu-1gb" 
+#   region     = var.region
+#   node_count = 1
 # }
+
+# # Tự động tạo danh sách các cơ sở dữ liệu dựa trên biến khai báo
+# resource "digitalocean_database_db" "all_databases" {
+#   for_each   = toset(var.databases)
+  
+#   cluster_id = digitalocean_database_cluster.postgres_db.id
+#   name       = each.key
+# }
+
+# # Output chuỗi kết nối tổng (Tùy chọn bỏ comment nếu cần lấy thông tin)
+# # output "db_connection_uri" {
+# #   value       = digitalocean_database_cluster.postgres_db.uri
+# #   sensitive   = true
+# #   description = "Chuỗi kết nối CSDL tổng."
+# # }
