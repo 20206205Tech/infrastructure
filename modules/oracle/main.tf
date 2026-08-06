a7af9b4f20325cce3ce2815a6777e23fee7dcdff
vuvannghiawork <vuvannghia.work@gmail.com>
2026-08-06 14:16:16 +0700

    8069

--- modules/oracle/main.tf
+++ modules/oracle/main.tf
@@ -178,4 +178,14 @@ resource "oci_core_default_security_list" "generated_oci_core_default_security_l
     }
   }
 
+  # Mở Port 8069 cho dịch vụ của bạn (TCP)
+  ingress_security_rules {
+    protocol = "6" # "6" đại diện cho giao thức TCP
+    source   = "0.0.0.0/0" # Cho phép truy cập từ mọi nguồn. Bạn có thể đổi thành IP cụ thể để bảo mật hơn.
+    tcp_options {
+      max = 8069
+      min = 8069
+    }
+  }
+
 }
